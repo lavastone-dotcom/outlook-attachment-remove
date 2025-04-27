f275cb269430beb3c41e1b132a6251e9f01107f7
lavastone-dotcom <saasbackup@lavastone.mu>
2025-04-27 15:56:21 +0400

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -17,8 +17,9 @@ Office.onReady(info => {
 
 function deleteSelected() {
     const selected = Array.from(document.querySelectorAll("input[type=checkbox]:checked")).map(e => e.value);
+
     if (selected.length === 0) {
-        alert("Please select at least one attachment to delete.");
+        showMessage("Please select at least one attachment to delete.", "error");
         return;
     }
 
@@ -27,4 +28,10 @@ function deleteSelected() {
     }
 
     selected.forEach(id => deleteAttachment(id));
+}
+
+function showMessage(message, type) {
+    const messageArea = document.getElementById("message-area");
+    messageArea.style.color = (type === "error") ? "red" : "green";
+    messageArea.innerText = message;
 }
